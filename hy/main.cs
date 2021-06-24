924952da85cb4c38efa3511e2481217b71d29f7d
BenToronto <83716543+BenToronto@users.noreply.github.com>
2021-06-24 17:08:51 -0400

    update

---
+++ hy/main.cs
@@ -0,0 +1,12 @@
+public override async Task SetParametersAsync(ParameterView parameters)
+{
+    for (int i = 0; i < length; i++)
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            try m
+        }
+    }
+
+    await base.SetParametersAsync(parameters);
+}
