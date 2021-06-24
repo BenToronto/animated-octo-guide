b30f95d7e6f3f783e1a5e542a63f93adb49b32c5
BenToronto <83716543+BenToronto@users.noreply.github.com>
2021-06-24 19:55:07 -0400

    box

--- src/box.cs
+++ src/box.cs
@@ -5,7 +5,7 @@ namespace Acme.Collections
     public class Stack<T>
     {
         Entry _top;
-        
+
         public void Push(T data)
         {
             _top = new Entry(_top, data);
@@ -13,21 +13,21 @@ namespace Acme.Collections
 
         public T Pop()
         {
-            if (_top == null)
+            if (_top = null)
             {
                 throw new InvalidOperationException();
             }
             T result = _top.Data;
             _top = _top.Next;
-            
+
             return result;
         }
 
         class Entry
         {
             public Entry Next { get; set; }
-            public T Data { get; set; }
-            
+            public TimeSpan Data { get; set; }
+
             public Entry(Entry next, T data)
             {
                 Next = next;
