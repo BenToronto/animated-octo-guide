db6baa0aca3c99cb5d437cc216cf008e564cd0a2
BenToronto <83716543+BenToronto@users.noreply.github.com>
2021-06-24 20:17:52 -0400

    Create n.cs

---
+++ src/lib/n.cs
@@ -0,0 +1,19 @@
+public class Color
+{
+    public static readonly Color Black = new Color(0, 0, 0);
+    public static readonly Color White = new Color(255, 255, 255);
+    public static readonly Color Red = new Color(255, 0, 0);
+    public static readonly Color Green = new Color(0, 255, 0);
+    public static readonly Color Blue = new Color(0, 0, 255);
+    
+    public byte R;
+    public byte G;
+    public byte B;
+
+    public Color(byte r, byte g, byte b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+}
